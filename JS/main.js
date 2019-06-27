342f20c402463b7c7f69cf17b753e5d0533ebd5f
Yuliia Senyk <senuk04@gmail.com>
2019-06-27 13:20:54 +0300

    gonna add react

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,34 @@ $("#lang").click(function(){
     $(".ukr").toggle();
     $(".eng").toggle();
  });
+
+ $("#plot").click(function(){
+ $('.secret').toggle();
+
+} ) 
+
+$('.media-body.skill-name').click(function(){
+// $('this.skill-description').toggle();
+$('.secret').toggle();
+
+})
   
+
+
+
+
+
  $('#toggle-box-checkbox').on('change', function(){
     if(this.checked){
       $('body').addClass('day');
       $('.navbar-dark').toggleClass('nav-ligth');
       $('.light-text').toggleClass('light-text').toggleClass('dark-text');
+      $('.mybackground').css('background-image', 'url("/pics/smokelight.png")');
     }else{
       $('body').removeClass('day');
       $('.navbar-dark').toggleClass('nav-ligth');
       $('.dark-text').toggleClass('light-text').toggleClass('dark-text');
+      $('.mybackground').css('background-image', 'url("/pics/smokedark.png")');
     }
   });
 
